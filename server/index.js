fa5a7edd2f92be5385af45bdff65c65a5f17cd9a
ST10268411 <126479662+ST10268411@users.noreply.github.com>
2025-09-07 19:52:40 +0200

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,22 @@
+//creating server
+require('dotenv').config();
+
+const express = require('express'); 
+const cors = require('cors'); 
+
+const authRoutes = require('./routes/auth.js');
+
+const app = express();
+const PORT = process.env.PORT || 5000; 
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true })); 
+
+app.get('/', (req, res) => {
+    res.send('hello!'); 
+});
+app.use('/auth',authRoutes)
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)); 
