b55a1efaaf3eb7eab471f90bc089025c85474dba
ST10268411 <126479662+ST10268411@users.noreply.github.com>
2025-09-19 11:31:21 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,28 +1,24 @@
 require('dotenv').config();
-const express = require('express'); 
-const cors = require('cors'); 
+const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const authRoutes = require('./routes/auth.js');
 
 const app = express();
-const PORT = process.env.PORT || 5000; 
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/auth', authRoutes);
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get(/.*/, (req, res) => {
-
-    res.sendFile(path.join(__dirname, '../client/build', 'index.js'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
 
 app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
-
-
-
